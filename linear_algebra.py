44a4a36f3ee9e62cf93f8169c7cbbdc63ebf2e92
tewat <hollywoodstreets23@gmail.com>
2022-03-21 14:28:40 +0300

    Clustering functions

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,5 +1,15 @@
+from __future__ import division
+import math
+
+from functools import reduce
+
+
 def vector_add(v, w):
     return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+def vector_sum(vectors):
+    return reduce(vector_add, vectors)
 
 
 def dot(v, w):
@@ -32,3 +42,19 @@ def vector_subtract(v, w):
 
 def scalar_multiply(c, v):
     return [c * v_i for v_i in v]
+
+
+# this isn't right if you don't from __future__ import division
+def vector_mean(vectors):
+    """compute the vector whose i-th element is the mean of the
+    i-th elements of the input vectors"""
+    n = len(vectors)
+    return scalar_multiply(1 / n, vector_sum(vectors))
+
+
+def squared_distance(v, w):
+    return sum_of_squares(vector_subtract(v, w))
+
+
+def distance(v, w):
+    return math.sqrt(squared_distance(v, w))
